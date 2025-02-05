7af402ef4a6f38b9f16f97083cf5ace92bb5de38
gayatri <patwegayatri5143@gmail.com>
2025-02-05 10:35:02 +0530

    add service

---
+++ Backend/Personal-Finance-Management/src/main/java/com/example/Personal/Finance/Management/Service/IncomeService.java
@@ -0,0 +1,10 @@
+package com.example.Personal.Finance.Management.Service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface IncomeService {
+    ResponseEntity<Double> postIncome();
+
+
+
+}
