0e3756a1caeabb61d3eaee6c763f094f02524192
Sarang2022344 <sarangkhode456@gmail.com>
2025-02-06 12:36:37 +0530

    Initial Commit

---
+++ Backend/Personal-Finance-Management/src/main/java/com/example/Personal/Finance/Management/Controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.Personal.Finance.Management.Controller;
+
+import com.example.Personal.Finance.Management.Service.AuthService;
+import com.example.Personal.Finance.Management.entity.LoginRequest;
+import com.example.Personal.Finance.Management.entity.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
