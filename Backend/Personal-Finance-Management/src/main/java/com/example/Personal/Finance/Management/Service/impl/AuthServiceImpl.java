9f30636391345abff7d27b5d74b4271b1c64a050
Sarang2022344 <sarangkhode456@gmail.com>
2025-02-06 12:38:36 +0530

    Second commit

---
+++ Backend/Personal-Finance-Management/src/main/java/com/example/Personal/Finance/Management/Service/impl/AuthServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.Personal.Finance.Management.Service.impl;
+
+import com.example.Personal.Finance.Management.Repository.UserRepository;
+import com.example.Personal.Finance.Management.Service.AuthService;
+import com.example.Personal.Finance.Management.entity.LoginRequest;
+import com.example.Personal.Finance.Management.entity.RegisterRequest;
+import com.example.Personal.Finance.Management.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class AuthServiceImpl implements AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public String register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists!");
+        }
+        User user = new User();
+        user.setName(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(user);
+        return "User registered successfully!";
+    }
+
+    @Override
+    public String login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found!"));
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials!");
+        }
+        return "Login successful!";
+    }
+}
